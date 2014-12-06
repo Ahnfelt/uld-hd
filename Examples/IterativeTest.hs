7148ad6367623648a00e3aab151ac3536d3be591
werk <mfwerk@gmail.com>
2014-12-06 21:37:25 +0100

    Zoom in mandelbrot with single precision is not too fun

--- Examples/IterativeTest.hs
+++ Examples/IterativeTest.hs
@@ -8,3 +8,4 @@ main :: IO ()
 main = generateHtml $ \t ->
     let a = scale (2**t) (2**t) $ translate (-0.743643887037158704752191506114774) 0.131825904205311970493132056385139 mandelbrot
     in a t
+                                                                                        git
